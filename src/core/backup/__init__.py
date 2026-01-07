2dc1870ce8007c94a149e1f9863fb12375fda3cf
Jonathas Motta <jonathasmrt@gmail.com>
2026-01-06 22:29:33 -0300

    docs(core): add purpose header to backup package

--- src/core/backup/__init__.py
+++ src/core/backup/__init__.py
@@ -1,9 +1,9 @@
 # ⌘
-#
 #  /fileknight/src/core/backup/__init__.py
 #
-#  Created by @jonathaxs on 2025-12-29.
+#  Purpose: Backup package marker (copy/mirror logic).
 #
+#  Created by @jonathaxs on 2025-12-29.
 # ⌘
 
 # Backup package
