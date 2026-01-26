4659d71e0a4c0e074eeae29497c61a8322f08adf
Jonathas Motta <jonathasmrt@gmail.com>
2026-01-26 00:32:22 -0300

    chore: move project files under fileknight folder

---
+++ fileknight/src/core/__init__.py
@@ -0,0 +1,9 @@
+# ⌘
+#  /fileknight/core/__init__.py
+#
+#  Purpose: Core package marker for shared FileKnight modules.
+#
+#  Created by @jonathaxs on 2025-12-25.
+# ⌘
+
+# FileKnight core package
---
+++ fileknight/src/core/backup/__init__.py
@@ -0,0 +1,9 @@
+# ⌘
+#  /fileknight/src/core/backup/__init__.py
+#
+#  Purpose: Backup package marker (copy/mirror logic).
+#
+#  Created by @jonathaxs on 2025-12-29.
+# ⌘
+
+# Backup package
