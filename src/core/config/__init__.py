b9c8d5b87d50eb9adab1c7cb6c998deaf4a10394
Jonathas Motta <jonathasmrt@gmail.com>
2026-01-26 00:34:25 -0300

    chore: remove legacy root files after move

--- src/core/config/__init__.py
+++
@@ -1,9 +0,0 @@
-# ⌘
-#  /fileknight/src/core/config/__init__.py
-#
-#  Purpose: Config package marker (load/save, validation, and config helpers).
-#
-#  Created by @jonathaxs on 2025-12-29.
-# ⌘
-
-# Config package
