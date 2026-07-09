9e1780a492b214f50c734ac74577ceaa827dd121
Jonathas Motta <jonathasmrt@gmail.com>
2026-07-08 23:17:19 -0300

    chore: removida a versao python antiga

--- fileknight/fileknight_app/src/core/__init__.py
+++
@@ -1,9 +0,0 @@
-# ⌘
-#  fileknight/core/__init__.py
-#
-#  Purpose: Core package marker for shared FileKnight modules.
-#
-#  Created by @jonathaxs on 2025-12-25.
-# ⌘
-
-# FileKnight core package
--- fileknight/fileknight_app/src/core/backup/__init__.py
+++
@@ -1,9 +0,0 @@
-# ⌘
-#  fileknight/src/core/backup/__init__.py
-#
-#  Purpose: Backup package marker (copy/mirror logic).
-#
-#  Created by @jonathaxs on 2025-12-29.
-# ⌘
-
-# Backup package
